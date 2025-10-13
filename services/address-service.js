9cf06167fab567b33373dfed1d4afc90b31c7ecc
Felipe Engler <felipepeengler@outlook.com>
2025-10-13 20:00:53 -0300

    fix user creation;

--- services/address-service.js
+++ services/address-service.js
@@ -4,7 +4,7 @@ const addressModel = require('../models/address-model');
 const {sequelizeError} = require('../errors/sequelizeError');
 
 const createAddress = async function (body, transaction) {
-  const address = await addressModel.create({uuid: uuid.v6(), ...body}, transaction)
+  const address = await addressModel.create({uuid: uuid.v6(), ...body}, {transaction})
     .catch(err => sequelizeError(err));
 
   return address.dataValues;
